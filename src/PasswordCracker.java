aca37bb7833e321c5dcd4d2cbee5cbc3b4fde447
Ethan <hendrixet@g.cofc.edu>
2018-02-05 22:35:49 -0500

    added num compare

--- src/PasswordCracker.java
+++ src/PasswordCracker.java
@@ -1,3 +1,18 @@
+/*
+*  Authors: McCardle, Patrick; Hendrix, Ethan
+*  Date: 02.05.18
+*  Dr. X
+*  CSCI 345
+*
+*  This project was created to test password cracking of the MD5 hashing algorithm
+*  Provided a file in the MD5 password file format user:hash[:other:options]
+*  this code can crack the hashes in that file using a standard common dictionary
+*  for this project we have used the dictionary in /usr/share/dict/words
+*  this is hard coded, but another dictionary could be used in the event that you wanted to
+*  it would parse that file and use the words in it just as well
+*
+* */
+
 import javax.xml.bind.DatatypeConverter;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
@@ -8,6 +23,7 @@ import java.io.FileNotFoundException;
 import java.io.File;
 import java.util.ArrayList;
 import java.security.MessageDigest;
+import java.nio.ByteBuffer;
 
 public class PasswordCracker {
     private static final String wordsPath = "/usr/share/dict/words";
@@ -22,6 +38,14 @@ public class PasswordCracker {
     public String getWordsPath()
     {
         return wordsPath;
+    }
+
+    public ArrayList<String> getHashes() {
+        return hashes;
+    }
+
+    public ArrayList<String> getPasswds() {
+        return passwds;
     }
 
     public void crack(String passFilePath)
@@ -44,9 +68,10 @@ public class PasswordCracker {
             MessageDigest md = MessageDigest.getInstance("MD5");
             for ( int i = 0 ; i <  hashes.size() ; i ++ )
             {
+                String hash = hashes.get(i);
                 while ( wordsSc.hasNextLine() ) {
 
-                    String hash = hashes.get(i);
+
                     String word = wordsSc.nextLine();
                     System.out.println(word);
 
@@ -57,9 +82,24 @@ public class PasswordCracker {
                     String hashedWord = DatatypeConverter.printHexBinary(digest);
                     System.out.println(hashedWord);
 
-                    if ( hash.compareTo(hashedWord) == 1) {
+                    if (hash.compareTo(hashedWord) == 1) {
                         passwds.add(word);
+                    } else {
+
                     }
+                }
+
+                for ( Integer j = 0 ; j <= 999999 ; j ++ ) {
+                    byte provider = j.byteValue();
+                    md.update(provider);
+                    byte[] digest = md.digest();
+
+                    String hashedInt = DatatypeConverter.printHexBinary(digest);
+
+                    if(hash.compareTo(hashedInt) == 1) {
+                        passwds.add(Integer.toString(j));
+                    }
+
                 }
             }
 
